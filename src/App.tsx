7aa9ce0e2e99e889b674f30c1e46fb7316adffa3
RaspRasp <108247612+RaspRasp@users.noreply.github.com>
2022-07-03 19:32:51 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "./styles.css";
+import {Header} from 
+export default function App() {
+  return (
+    <div className="App">
+     <header/>
+    </div>
+}
---
+++ src/Header.tsx
@@ -0,0 +1,6 @@
+<header>
+  <h1 className="title">Akazukin</h1>
+  <ul className="menu">
+    <li></li>
+  </ul>
+</header>;
